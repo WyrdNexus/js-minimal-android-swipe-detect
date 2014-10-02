8b6616c6b715ab1a24f1aaabe38a29b06e84f572
Tony Chiboucas <WyrdNexus@gmail.com>
2014-10-02 16:31:51 -0700

    Callback functions for swipe directions

--- uiSwipe.js
+++ uiSwipe.js
@@ -1,18 +1,36 @@
-// JavaScript Document
-// fn = function( dir, dist )
-//  dir:
-//	       +- pi
-//   -pi/2  x   pi/2
-//          0
-//	dist:
-//		pixels
+ /**
+  *  uiSwipe - for touch and mobile
+  *  
+  *  Parameter: (object) swipeActions
+  *     swipeUp: function(){}   ,
+  *     swipeDown: function(){} ,
+  *     swipeLeft: function(){} ,
+  *     swipeUp: function(){}   ,
+  *     swipeAll: function( direction, distance )
+  */
 
-function uiSwipe(fn){
+
+function uiSwipe(swipeActions){
+	
 	this.pStart = [0,0];
 	this.pStop = [0,0];
 	this.distance = 0;
 	this.direction = 0;
+	this.minSwipeDist = document.documentElement.clientWidth/2;
+	this.blnL = (typeof swipeActions.swipeLeft   === "function");
+	this.blnR = (typeof swipeActions.swipeRight  === "function");
+	this.blnU = (typeof swipeActions.swipeUp     === "function");
+	this.blnD = (typeof swipeActions.swipeDown   === "function");
+	this.blnA = (typeof swipeActions.swipeAll    === "function");
+	
 	var parentUI = this;
+	document.addEventListener('touchstart', function(e){ parentUI.eStart(e); }, false);
+	document.addEventListener('touchend', function(e)	{ parentUI.eEnd(e); }, false);
+	//document.body.addEventListener('mousedown', function(e)	{ parentUI.eStart(e); }, false);
+	//document.body.addEventListener('mouseup', function(e)	{ parentUI.eEnd(e); }, false);
+
+	$("#tester2").html("uiSwipe");
+	
 	
 	this.eStart = function(event){
 		if (typeof event['targetTouches'] !== "undefined"){
@@ -21,6 +39,7 @@ function uiSwipe(fn){
 		} else {
 			this.pStart = [event.screenX, event.screenY];
 		}
+		$("#tester2").html("eStart ("+ this.pStart[0] +","+ this.pStart[1] +")");
 	};
 	
 	this.eEnd = function(event){
@@ -32,15 +51,12 @@ function uiSwipe(fn){
 			this.pStop = [event.screenX, event.screenY];
 			this.check(1);	
 		}
+		
+		$("#tester3").html("eEnd ("+ this.pStop[0] +","+ this.pStop[1] +")");
 	};
 	
-	document.body.addEventListener('touchstart', function(e){ parentUI.eStart(e); }, false);
-	document.body.addEventListener('touchend', function(e){ parentUI.eEnd(e); }, false);
-	document.body.addEventListener('mousedown', function(e){ parentUI.eStart(e); }, false);
-	document.body.addEventListener('mouseup', function(e){ parentUI.eEnd(e); }, false);
-	
 	this.check = function(good){
-		
+		$("#tester4").html("check "+good);
 		if (good) {
 			var x = (this.pStop[0] - this.pStart[0]); // change in x
 			var y = (this.pStop[1] - this.pStart[1]); // change in y
@@ -56,7 +72,35 @@ function uiSwipe(fn){
 	};
 	
 	this.final = function(){
-		fn(this.direction, this.distance);
+		$("#tester4").html("dir:"+ this.direction.toFixed(2) +" dist:"+ this.distance.toFixed(2));
+		
+		if(  this.distance > this.minSwipeDist  ) {
+			if (         this.blnL  &&  Math.abs(this.direction) > 2.3  ) {
+			//	Left
+				swipeActions.swipeLeft();
+				
+			} else if (  this.blnR  &&  Math.abs(this.direction) < 0.8  ) {
+			//	Right
+				swipeActions.swipeRight();
+				
+			} else if (  this.blnU  &&  this.direction < 0              ) {
+			//	Up
+				swipeActions.swipeUp();
+				
+			} else if (  this.blnD  &&  this.direction > 0              ) {
+			//	Down
+				swipeActions.swipeDown();
+				
+			} 
+			
+			if ( this.blnA ) {
+			//  All
+				swipeActions.swipeAll( this.direction, this.distance );
+				
+			}
+			$("#tester1").html("DIR: "+ this.direction +"\n<br>DIST: "+ Math.round(this.distance));
+		}
+		
 		this.clear();	
 	};
 	
@@ -66,4 +110,15 @@ function uiSwipe(fn){
 		this.direction = 0;
 		this.distance = 0;
 	};
+}
+
+
+function objExp( obj ) {
+    var str = '';
+    for (var p in obj) {
+        if (obj.hasOwnProperty(p)) {
+            str += p + '::' + obj[p] + '\n<br>';
+        }
+    }
+    return str;
 }
